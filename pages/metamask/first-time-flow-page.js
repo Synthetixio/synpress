66188f479abfbf5f6111a36002f8d20521ba40f7
Jakub Mucha <jakub.mucha@icloud.com>
2021-08-09 21:31:57 +0200

    po: refactor first time flow page
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- pages/metamask/first-time-flow-page.js
+++ pages/metamask/first-time-flow-page.js
@@ -1,13 +1,25 @@
 const app = '#app-content .app';
-
 const welcomePage = '.welcome-page';
 const confirmButton = `${welcomePage} .first-time-flow__button`;
+module.exports.welcomePageElements = {
+  app,
+  welcomePage,
+  confirmButton,
+};
 
 const firstTimeFlowPage = '.first-time-flow';
 const importWalletButton = `${firstTimeFlowPage} .first-time-flow__button`;
+module.exports.firstTimeFlowPageElements = {
+  firstTimeFlowPage,
+  importWalletButton,
+};
 
 const metametricsPage = '.metametrics-opt-in';
 const optOutAnalyticsButton = `${metametricsPage} [data-testid="page-container-footer-cancel"]`;
+module.exports.metametricsPageElements = {
+  metametricsPage,
+  optOutAnalyticsButton,
+};
 
 const firstTimeFlowFormPage = '.first-time-flow__form';
 const secretWordsInput = `${firstTimeFlowFormPage} .first-time-flow__seedphrase input`;
@@ -15,29 +27,6 @@ const passwordInput = `${firstTimeFlowFormPage} #password`;
 const confirmPasswordInput = `${firstTimeFlowFormPage} #confirm-password`;
 const termsCheckbox = `${firstTimeFlowFormPage} .first-time-flow__terms`;
 const importButton = `${firstTimeFlowFormPage} .first-time-flow__button`;
-
-const endOfFlowPage = '.end-of-flow';
-const allDoneButton = `${endOfFlowPage} .first-time-flow__button`;
-
-const revealSeedPage = '.reveal-seed-phrase';
-const remindLaterButton = `${revealSeedPage} .first-time-flow__button`;
-
-module.exports.welcomePageElements = {
-  app,
-  welcomePage,
-  confirmButton,
-};
-
-module.exports.firstTimeFlowPageElements = {
-  firstTimeFlowPage,
-  importWalletButton,
-};
-
-module.exports.metametricsPageElements = {
-  metametricsPage,
-  optOutAnalyticsButton,
-};
-
 module.exports.firstTimeFlowFormPageElements = {
   firstTimeFlowFormPage,
   secretWordsInput,
@@ -47,11 +36,15 @@ module.exports.firstTimeFlowFormPageElements = {
   importButton,
 };
 
+const endOfFlowPage = '.end-of-flow';
+const allDoneButton = `${endOfFlowPage} .first-time-flow__button`;
 module.exports.endOfFlowPageElements = {
   endOfFlowPage,
   allDoneButton,
 };
 
+const revealSeedPage = '.reveal-seed-phrase';
+const remindLaterButton = `${revealSeedPage} .first-time-flow__button`;
 module.exports.revealSeedPageElements = {
   revealSeedPage,
   remindLaterButton,
