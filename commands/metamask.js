0ed24d9da82992857dd4aab96af8d7f22007c3ab
Jakub Mucha <jakub.mucha@icloud.com>
2021-10-12 12:53:44 +0200

    fix: linting issues
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- commands/metamask.js
+++ commands/metamask.js
@@ -386,7 +386,10 @@ module.exports = {
   },
   initialSetup: async ({ secretWordsOrPrivateKey, network, password }) => {
     const isCustomNetwork =
-      (process.env.NETWORK_NAME && process.env.RPC_URL && process.env.CHAIN_ID) || typeof(network) == 'object';
+      (process.env.NETWORK_NAME &&
+        process.env.RPC_URL &&
+        process.env.CHAIN_ID) ||
+      typeof network == 'object';
 
     await puppeteer.init();
     await puppeteer.assignWindows();
