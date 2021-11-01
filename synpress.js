9279dcf441ddc0d442e8fd39d90c52c767e6128e
Jakub Mucha <jakub.mucha@icloud.com>
2021-11-01 23:49:39 +0100

    add better error handling in case of missing env vars
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- synpress.js
+++ synpress.js
@@ -4,6 +4,33 @@ const program = require('commander');
 const { run, open } = require('./launcher');
 const { version } = require('./package.json');
 
+if (
+  !process.env.SECRET_WORDS &&
+  !process.env.PRIVATE_KEY &&
+  !process.env.SKIP_METAMASK_SETUP
+) {
+  throw new Error(
+    'Please provide SECRET_WORDS or PRIVATE_KEY environment variable',
+  );
+}
+
+if (process.env.RPC_URL || process.env.CHAIN_ID) {
+  if (!process.env.RPC_URL) {
+    throw new Error('Please provide RPC_URL environment variable');
+  } else if (!process.env.CHAIN_ID) {
+    throw new Error('Please provide CHAIN_ID environment variable');
+  }
+
+  if (
+    !process.env.RPC_URL.startsWith('http://') && //DevSkim: ignore DS137138
+    !process.env.RPC_URL.startsWith('https://')
+  ) {
+    throw new Error(
+      'RPC_URL environment variable should start with "http://" or "https://"', //DevSkim: ignore DS137138
+    );
+  }
+}
+
 program.version(version, '-v, --version');
 
 program
