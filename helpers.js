c3a418e15d1b8e97f88fbad4c3d361b02820036b
Jakub Mucha <jakub.mucha@icloud.com>
2022-10-07 20:58:23 +0200

    feat: added logging to helpers
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- helpers.js
+++ helpers.js
@@ -1,3 +1,4 @@
+const log = require('debug')('synpress:helpers');
 const axios = require('axios');
 const fs = require('fs').promises;
 const path = require('path');
@@ -10,6 +11,10 @@ let isTestnet = false;
 
 module.exports = {
   setNetwork: network => {
+    typeof network === 'object'
+      ? log(`Setting network to ${JSON.stringify(network)}`)
+      : log(`Setting network to ${network}`);
+
     if (network === 'mainnet') {
       networkName = 'mainnet';
       networkId = 1;
@@ -38,7 +43,9 @@ module.exports = {
     // todo: handle a case when setNetwork() is triggered by changeNetwork() with a string of already added custom networks
   },
   getNetwork: () => {
-    return { networkName, networkId, isTestnet };
+    const networkData = { networkName, networkId, isTestnet };
+    log(`Current network data: ${networkData}`);
+    return networkData;
   },
   getSynpressPath: () => {
     if (process.env.SYNPRESS_LOCAL_TEST) {
@@ -49,23 +56,29 @@ module.exports = {
   },
   createDirIfNotExist: async path => {
     try {
+      log(`Checking if directory exists on path: ${path}`);
       await fs.access(path);
+      return true;
     } catch (e) {
       if (e.code === 'ENOENT') {
+        log(`Creating directory as it doesn't exist..`);
         await fs.mkdir(path);
+        return true;
       } else {
         throw new Error(
-          `[prepareMetamask] Unhandled error from fs.access() with following error:\n${e}`,
+          `[createDirIfNotExist] Unhandled error from fs.access() with following error:\n${e}`,
         );
       }
     }
   },
   checkDirOrFileExist: async path => {
     try {
+      log(`Checking if directory exists on path: ${path}`);
       await fs.access(path);
       return true;
     } catch (e) {
       if (e.code === 'ENOENT') {
+        log(`Directory or file doesn't exist`);
         return false;
       } else {
         throw new Error(
@@ -75,6 +88,7 @@ module.exports = {
     }
   },
   getMetamaskReleases: async version => {
+    log(`Trying to find metamask version ${version} in GitHub releases..`);
     let filename;
     let downloadUrl;
     let tagName;
@@ -87,10 +101,16 @@ module.exports = {
         filename = response.data[0].assets[0].name;
         downloadUrl = response.data[0].assets[0].browser_download_url;
         tagName = response.data[0].tag_name;
+        log(
+          `Metamask version found! Filename: ${filename}; Download url: ${downloadUrl}; Tag name: ${tagName}`,
+        );
       } else if (version) {
         filename = `metamask-chrome-${version}.zip`;
         downloadUrl = `https://github.com/MetaMask/metamask-extension/releases/download/v${version}/metamask-chrome-${version}.zip`;
         tagName = `metamask-chrome-${version}`;
+        log(
+          `Metamask version found! Filename: ${filename}; Download url: ${downloadUrl}; Tag name: ${tagName}`,
+        );
       }
       return {
         filename,
@@ -105,6 +125,9 @@ module.exports = {
   },
   download: async (url, destination) => {
     try {
+      log(
+        `Trying to download and extract file from: ${url} to following path: ${destination}`,
+      );
       await download(url, destination, { extract: true });
     } catch (e) {
       throw new Error(
@@ -130,6 +153,8 @@ module.exports = {
     );
     if (!metamaskDirectoryExists && !metamaskManifestFileExists) {
       await module.exports.download(release.downloadUrl, metamaskDirectory);
+    } else {
+      log('Metamask is already downloaded');
     }
     return metamaskDirectory;
   },
