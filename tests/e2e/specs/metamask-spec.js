44b14d9839cc11c5702a4e964954a004ad7272e8
Jakub Mucha <jakub.mucha@icloud.com>
2022-10-16 18:29:30 +0200

    e2e: change network to localhost while testing with anvil
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- tests/e2e/specs/metamask-spec.js
+++ tests/e2e/specs/metamask-spec.js
@@ -233,6 +233,7 @@ describe('Metamask', () => {
           // 'test test test test test test test test test test test junk'
           '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
         );
+        cy.changeMetamaskNetwork('localhost');
       } else {
         cy.importMetamaskAccount(Cypress.env('PRIVATE_KEY_WITH_FUNDS'));
       }
