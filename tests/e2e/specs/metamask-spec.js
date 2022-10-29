e62799ffd670978a47b2958929c0bfef7f805fb6
Jakub Mucha <jakub.mucha@icloud.com>
2022-10-29 13:02:51 +0100

    e2e: define secret words for setupmetamask
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- tests/e2e/specs/metamask-spec.js
+++ tests/e2e/specs/metamask-spec.js
@@ -3,7 +3,11 @@ describe('Metamask', () => {
   context('Test commands', () => {
     // todo: clear the state of extension and test different combinations of setupMetamask with private key & custom network
     it(`setupMetamask should finish metamask setup using secret words`, () => {
-      cy.setupMetamask().then(setupFinished => {
+      cy.setupMetamask(
+        'test test test test test test test test test test test junk',
+        'goerli',
+        'Tester@1234',
+      ).then(setupFinished => {
         expect(setupFinished).to.be.true;
       });
     });
