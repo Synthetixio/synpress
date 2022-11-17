a5be9ccec4b81ba1d95b35c97358dbf52e7aa983
Jakub Mucha <jakub.mucha@icloud.com>
2022-11-17 01:28:22 +0000

    e2e: siwe
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

---
+++ tests/e2e/enable-later/siwe-spec.js
@@ -0,0 +1,21 @@
+describe('SIWE', () => {
+  context('Test commands', () => {
+    it(`acceptMetamaskAccess should work with SIWE signature request after connecting a wallet`, () => {
+      cy.setupMetamask(
+        'test test test test test test test test test test test junk',
+        'mainnet',
+        'Tester@1234',
+      );
+      cy.origin('https://login.xyz', () => {
+        cy.visit('/');
+        cy.get('[data-action="submit"]').click();
+        cy.get(
+          '#WEB3_CONNECT_MODAL_ID .web3modal-provider-wrapper:nth-child(1)',
+        ).click();
+      });
+      cy.acceptMetamaskAccess({ signInSignature: true }).then(connected => {
+        expect(connected).to.be.true;
+      });
+    });
+  });
+});
