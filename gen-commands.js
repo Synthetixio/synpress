9329ac06b2684095ca900dc8161b8c417b679f5a
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-02-23 16:41:50 +0200

    refactor: create util function & add descriptive comments

--- gen-commands.js
+++ gen-commands.js
@@ -1,45 +1,36 @@
 /**
  * A script to automatically generate Synpress Commands (API) documentation from
  * types definition.
+ *
+ * This script make the following assumption about the types file
+ * 1. All comments are multiline.
+ * 2. First line of the comment is where the description is located
  */
 
-// Match comments + type defs https://regex101.com/r/KkO2II/1
-// Math comments only: https://regex101.com/r/CKPtWg/1
-
 const fs = require('node:fs');
-
 const TYPES_FILE = 'support/index.d.ts';
-const OUTPUT_FILE = 'docs/synpress-commands.md';
+const OUTPUT_FILE = 'docs/synpress-commands.test.md';
+/**
+ * A regex to extract comment + function signature for every command.
+ * Live Demo: https://regex101.com/r/KkO2II/1
+ */
+const BLOCKS_REGEX = /\/\*\*[\s\S]*?>;/g;
+/**
+ * A regex to extract the comment only.
+ * Live Demo: https://regex101.com/r/CKPtWg/1
+ */
+const MULTILINE_COMMENT_REGEX = /\/\*\*[\s\S]*?\/\n/g;
 
 // Entry point
 main();
 function main() {
-  const code = fs.readFileSync(TYPES_FILE).toString('utf-8');
-
-  const reBlocks = /\/\*\*[\s\S]*?>;/g;
-  const reComment = /\/\*\*[\s\S]*?\/\n/g;
-
-  const blocks = code.match(reBlocks);
-  if (!blocks) throw new Error(`Unable to parse types`);
-
-  const synpressCommands = blocks.map(block => {
-    const commentMatchRes = block.match(reComment);
-    if (!commentMatchRes) throw new Error('Missing or invalid comment');
-    const fullComment = commentMatchRes[0];
-    const firstLine = fullComment.split('\n')[1].trim().slice(2);
-    const functionSignature = block.replace(fullComment, '').trim();
-    const functionName = functionSignature.split('(')[0].trim();
-
-    return {
-      description: firstLine,
-      commandSig: functionSignature,
-      commandName: functionName,
-    };
-  });
-
-  saveToFile(intoMarkdownString(synpressCommands));
+  const types = loadTypes();
+  const synpressCommands = parseTypes(types);
+  const markdown = intoMarkdownString(synpressCommands);
+  saveToFile(markdown);
 }
 
+// Remove unnecessary indentation.
 function formatCommandSig(commandSig) {
   return commandSig
     .split('\n')
@@ -64,8 +55,49 @@ function intoMarkdownString(commands) {
 }
 
 function saveToFile(markdownStr) {
-  if (!fs.existsSync(OUTPUT_FILE))
-    throw new Error(`{${OUTPUT_FILE}} file not found`);
-
   fs.writeFileSync(OUTPUT_FILE, markdownStr);
+}
+
+function loadTypes() {
+  if (!fs.existsSync(TYPES_FILE))
+    throw new Error(`{${TYPES_FILE}} file not found`);
+  return fs.readFileSync(TYPES_FILE).toString('utf-8');
+}
+
+function parseTypes(code) {
+  // Block = comment + function signature
+  // Block Example
+  // -------------------- Start --------------------
+  // /**
+  //    * Assert that element is within viewport
+  //    * @example
+  //    * cy.get('selector').isWithinViewport()
+  //    * cy.get('selector').isWithinViewport(800, 600)
+  //    */
+  //  isWithinViewport(
+  //   viewportWidth: number,
+  //   viewportHeight: number,
+  // ): Chainable<Subject>;
+  // -------------------- End --------------------
+  const blocks = code.match(BLOCKS_REGEX);
+  if (!blocks) throw new Error(`Unable to parse types`);
+
+  const synpressCommands = blocks.map(block => {
+    // Extract the comment from the block
+    const commentMatchRes = block.match(MULTILINE_COMMENT_REGEX);
+    if (!commentMatchRes) throw new Error('Missing or invalid comment');
+
+    const fullComment = commentMatchRes[0];
+    const firstLine = fullComment.split('\n')[1].trim().slice(2);
+    const functionSignature = block.replace(fullComment, '').trim();
+    const functionName = functionSignature.split('(')[0].trim();
+
+    return {
+      description: firstLine,
+      commandSig: functionSignature,
+      commandName: functionName,
+    };
+  });
+
+  return synpressCommands;
 }
