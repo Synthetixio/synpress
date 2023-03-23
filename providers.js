0878ca1852879e027962fcee945ee281bdbfe326
Maxim Geerinck <geerinck.maxim@gmail.com>
2023-03-23 13:26:09 +0100

    v4.0.0-alpha.1

--- providers.js
+++ providers.js
@@ -1,7 +1,7 @@
 module.exports = {
   getProviders: providersConfig => {
     const providers = [];
-    const tempProviders = (providersConfig ?? '').split(',');
+    const tempProviders = (providersConfig || '').split(',');
     for (const provider of tempProviders) {
       const [providerName, providerVersion] = provider.split('@'); // can accept names like phantom@10.25.0
       providers.push({ name: providerName, version: providerVersion });
