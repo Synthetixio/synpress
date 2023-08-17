fe02c5d366396c856b40e8e1200acd16cddd293f
r3kt.eth <so@r3kt.io>
2023-08-17 19:34:07 +0200

    fix lint
    Signed-off-by: r3kt.eth <so@r3kt.io>

--- helpers.js
+++ helpers.js
@@ -238,9 +238,8 @@ module.exports = {
 
     await module.exports.createDirIfNotExist(downloadsDirectory);
     const metamaskDirectory = path.join(downloadsDirectory, release.tagName);
-    const metamaskDirectoryExists = await module.exports.checkDirOrFileExist(
-      metamaskDirectory,
-    );
+    const metamaskDirectoryExists =
+      await module.exports.checkDirOrFileExist(metamaskDirectory);
     const metamaskManifestFilePath = path.join(
       downloadsDirectory,
       release.tagName,
