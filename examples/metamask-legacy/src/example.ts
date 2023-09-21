ba9ba7475d6c2d1d0dd8ec975be3ec373b5480b5
Daniel Izdebski <spidiritsu@gmail.com>
2023-09-21 03:53:34 +0200

    Extend example

--- examples/metamask-legacy/src/example.ts
+++ examples/metamask-legacy/src/example.ts
@@ -1,3 +1,7 @@
-import { helpers } from 'metamask-legacy'
+import { helpers, metamaskPages } from 'metamask-legacy'
 
 console.log(helpers.getCurrentNetwork())
+console.log(
+  'Example selector:',
+  metamaskPages.mainPage.mainPageElements.importAccount.importButton
+)
