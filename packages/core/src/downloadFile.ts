92019e9d83eb81af604b0a0e2f9c8155d57e2808
Daniel Izdebski <spidiritsu@gmail.com>
2023-10-02 20:18:04 +0200

    ✨ chore(core): Add `downloadFile` function (#897)

---
+++ packages/core/src/downloadFile.ts
@@ -0,0 +1,63 @@
+import path from 'node:path'
+import axios from 'axios'
+import fs from 'fs-extra'
+
+type DownloaderOptions = {
+  url: string
+  outputDir: string
+  fileName: string
+  overrideFile?: boolean
+}
+
+type DownloadFileResult = {
+  filePath: string
+  downloadSkipped: boolean
+}
+
+export async function downloadFile(options: DownloaderOptions) {
+  const { url, outputDir, fileName, overrideFile } = options
+
+  const returnPromise = new Promise<DownloadFileResult>((resolve, reject) => {
+    const filePath = path.join(outputDir, fileName)
+
+    const fileExists = fs.existsSync(filePath)
+    if (fileExists && !overrideFile) {
+      resolve({
+        filePath,
+        downloadSkipped: true
+      })
+    }
+
+    axios
+      .get(url, {
+        responseType: 'stream'
+      })
+      .then((response) => {
+        const writer = fs.createWriteStream(filePath)
+
+        response.data.pipe(writer)
+
+        writer.on('finish', () => {
+          resolve({
+            filePath,
+            downloadSkipped: false
+          })
+        })
+        writer.on('error', (error) => {
+          // TODO: Handle errors in a more sophisticated way
+          reject(new Error(`[Writer] ${error.message}`))
+        })
+      })
+      .catch((error) => {
+        // TODO: Handle errors in a more sophisticated way
+        reject(new Error(`[Axios] ${error.message}`))
+      })
+  })
+
+  // TODO: This is a workaround to handle errors from both `writer` and `axios` and get 100% test coverage.
+  return returnPromise.catch((error: Error) => {
+    throw new Error(
+      `[DownloadFile] Error downloading the file - ${error.message}`
+    )
+  })
+}
