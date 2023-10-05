dc1f73fb01ac9869daa1e90f2519b672b42626ec
Daniel Izdebski <spidiritsu@gmail.com>
2023-10-05 16:02:05 +0200

    ✅ test(metamask): Add E2E test for `prepareExtension` (#915)

---
+++ wallets/metamask/test/e2e/prepareExtension.spec.ts
@@ -0,0 +1,45 @@
+import { type Page, chromium, test as base } from '@playwright/test'
+import { prepareExtension } from '../../src/prepareExtension'
+
+const test = base.extend({
+  context: async ({ context: _ }, use) => {
+    const metamaskPath = await prepareExtension()
+
+    const browserArgs = [
+      `--disable-extensions-except=${metamaskPath}`,
+      `--load-extension=${metamaskPath}`
+    ]
+
+    if (process.env.HEADLESS) {
+      browserArgs.push('--headless=new')
+    }
+
+    const context = await chromium.launchPersistentContext('', {
+      headless: false,
+      args: browserArgs
+    })
+
+    try {
+      await context.waitForEvent('page', { timeout: 5000 })
+    } catch {
+      throw new Error('[FIXTURE] MetaMask extension did not load in time')
+    }
+
+    await use(context)
+  },
+  page: async ({ context }, use) => {
+    const metamaskOnboardingPage = context.pages()[1] as Page
+    await use(metamaskOnboardingPage)
+  }
+})
+
+const { describe, expect } = test
+
+describe('prepareExtension', () => {
+  test('onboarding page opens up', async ({ page }) => {
+    await expect(page).toHaveTitle('MetaMask')
+    await expect(
+      page.getByRole('heading', { name: "Let's get started" })
+    ).toBeVisible()
+  })
+})
