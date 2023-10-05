34762c5404840c3696d90d7c9029a2c801ec1a0e
Daniel Izdebski <spidiritsu@gmail.com>
2023-10-05 21:14:52 +0200

    ✨ feat(metamask): Add support for importing seed phrase

---
+++ wallets/metamask/src/selectors/onboarding/getStartedPage.ts
@@ -0,0 +1,6 @@
+import { createDataTestSelector } from '../../utils/selectors/createDataTestSelector'
+
+export default {
+  createNewWallet: createDataTestSelector('onboarding-create-wallet'),
+  importWallet: createDataTestSelector('onboarding-import-wallet')
+}
---
+++ wallets/metamask/src/selectors/onboarding/secretRecoveryPhrasePage.ts
@@ -0,0 +1,22 @@
+import { createDataTestSelector } from '../../utils/selectors/createDataTestSelector'
+
+const recoveryStep = {
+  selectNumberOfWordsDropdown: '.import-srp__number-of-words-dropdown > .dropdown__select',
+  selectNumberOfWordsOption: (option: number | string) => `${option}`,
+  secretRecoveryPhraseWord: (index: number) => createDataTestSelector(`import-srp__srp-word-${index}`),
+  confirmSecretRecoveryPhraseButton: createDataTestSelector('import-srp-confirm'),
+  error: '.actionable-message.actionable-message--danger.import-srp__srp-error > .actionable-message__message'
+}
+
+const passwordStep = {
+  passwordInput: createDataTestSelector('create-password-new'),
+  confirmPasswordInput: createDataTestSelector('create-password-confirm'),
+  acceptTermsCheckbox: createDataTestSelector('create-password-terms'),
+  importWalletButton: createDataTestSelector('create-password-import'),
+  error: `${createDataTestSelector('create-password-confirm')} + h6`
+}
+
+export default {
+  recoveryStep,
+  passwordStep
+}
