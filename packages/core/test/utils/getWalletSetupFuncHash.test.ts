c21cdb69db9955c1861c52a9953e093715b4ddbb
Daniel Izdebski <spidiritsu@gmail.com>
2023-10-24 01:43:10 +0200

    ✨ feat(core): Add `getWalletSetupFuncHash` function

---
+++ packages/core/test/utils/getWalletSetupFuncHash.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from 'vitest'
+import { WALLET_SETUP_FUNC_HASH_LENGTH, getWalletSetupFuncHash } from '../../src/utils/getWalletSetupFuncHash'
+
+const EXPECTED_HASH = '9638e9a1f0a7719fa148'
+
+const testFunction = (name: string) => {
+  return `Hello ${name}!`
+}
+
+describe('getWalletSetupFuncHash', () => {
+  it('throws an error if esbuild transformation fails', async () => {
+    const incorrectFunctionObject = {
+      toString: () => null
+      // biome-ignore lint/suspicious/noExplicitAny: any type here is intentional
+    } as any
+
+    expect(() => getWalletSetupFuncHash(incorrectFunctionObject)).toThrowError('Transform failed with 1 error')
+  })
+
+  it('returns hash', async () => {
+    const hash = getWalletSetupFuncHash(testFunction)
+    expect(hash).toEqual(EXPECTED_HASH)
+  })
+
+  it('returns hash of a correct length', async () => {
+    const hash = getWalletSetupFuncHash(testFunction)
+
+    // We multiply by 2 because the hash is in a hex format, i.e. each byte is represented by 2 characters.
+    expect(hash.length).toEqual(2 * WALLET_SETUP_FUNC_HASH_LENGTH)
+  })
+})
