fd8edb1cb67d171351cb707fa3a4aa26aba47f66
Daniel Izdebski <spidiritsu@gmail.com>
2023-10-24 02:19:40 +0200

    ✨ feat(core): Add `getWalletSetupFuncHash` function (#947)

---
+++ packages/core/src/utils/getWalletSetupFuncHash.ts
@@ -0,0 +1,25 @@
+import { createHash } from 'node:crypto'
+import esbuild from 'esbuild'
+
+// Same length as the file part (first part before the `-`) of a Playwright Test ID.
+export const WALLET_SETUP_FUNC_HASH_LENGTH = 10
+
+// biome-ignore lint/suspicious/noExplicitAny: any type here is intentional
+type AnyFunction = (...args: any) => any
+
+export function getWalletSetupFuncHash(walletSetupFunc: AnyFunction) {
+  // This transformation is necessary because a user could end up using a different execution engine than Playwright.
+  // Different execution engines -> different codes -> different hashes.
+  const { code } = esbuild.transformSync(walletSetupFunc.toString(), {
+    format: 'esm',
+    drop: ['console', 'debugger'],
+    loader: 'ts',
+    logLevel: 'silent'
+  })
+
+  const hash = createHash('shake256', {
+    outputLength: WALLET_SETUP_FUNC_HASH_LENGTH
+  })
+
+  return hash.update(code).digest('hex')
+}
