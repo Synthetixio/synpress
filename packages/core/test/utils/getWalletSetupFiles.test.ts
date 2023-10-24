cab988c7de74221262ff89698ac049c4bc413db5
Daniel Izdebski <spidiritsu@gmail.com>
2023-10-24 22:32:36 +0200

    ✨ feat(core): Add `getWalletSetupFiles` function (#949)

---
+++ packages/core/test/utils/getWalletSetupFiles.test.ts
@@ -0,0 +1,68 @@
+import path from 'node:path'
+import { fs, vol } from 'memfs'
+import { afterAll, afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { getWalletSetupFiles } from '../../src/utils/getWalletSetupFiles'
+
+const ROOT_DIR = '/tmp'
+const DUMMY_CONTENT = 'Hello world! 👋'
+
+vi.mock('fs-extra', async () => {
+  return {
+    default: fs.promises
+  }
+})
+
+async function createFiles(fileNames: string[]) {
+  for (const fileName of fileNames) {
+    const filePath = path.join(ROOT_DIR, fileName)
+    await fs.promises.writeFile(filePath, DUMMY_CONTENT)
+  }
+}
+
+describe('getWalletSetupFiles', () => {
+  afterAll(() => {
+    vi.resetAllMocks()
+  })
+
+  beforeEach(async () => {
+    vol.mkdirSync(ROOT_DIR)
+    await createFiles([
+      'quack',
+      'quack.js',
+      'quack.ts',
+      'quack.tsx',
+      'quack.setup',
+      'quack.setup.js',
+      'quack.setup.ts',
+      'quack.setup.tsx'
+    ])
+  })
+
+  afterEach(() => {
+    vol.reset() // Clear the in-memory file system after each test
+  })
+
+  it('throws if the target directory does not exist', async () => {
+    const nonExistentDirPath = path.join(ROOT_DIR, 'non-existent-dir')
+    await expect(getWalletSetupFiles(nonExistentDirPath)).rejects.toThrowError(
+      `[GetWalletSetupFiles] Wallet setup directory does not exist at ${nonExistentDirPath}`
+    )
+  })
+
+  it('throws if no setup files are found in the target directory', async () => {
+    const emptyDirPath = path.join(ROOT_DIR, 'empty-dir')
+    vol.mkdirSync(emptyDirPath)
+
+    await expect(getWalletSetupFiles(emptyDirPath)).rejects.toThrowError(
+      `[GetWalletSetupFiles] No wallet setup files found at ${emptyDirPath}`
+    )
+  })
+
+  it('returns only valid setup files', async () => {
+    const setupFiles = await getWalletSetupFiles(ROOT_DIR)
+
+    expect(setupFiles).toHaveLength(2)
+    expect(setupFiles).toContain('quack.setup.js')
+    expect(setupFiles).toContain('quack.setup.ts')
+  })
+})
