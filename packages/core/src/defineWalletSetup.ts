a3c535eb298e0aac25f7e5790b34b3f8f89c80f8
Daniel Izdebski <spidiritsu@gmail.com>
2023-10-24 18:41:53 +0200

    ✨ feat(core): Add `defineWalletSetup` function (#948)

---
+++ packages/core/src/defineWalletSetup.ts
@@ -0,0 +1,14 @@
+import type { BrowserContext, Page } from 'playwright-core'
+import { getWalletSetupFuncHash } from './utils/getWalletSetupFuncHash'
+
+export type WalletSetupFunction = (context: BrowserContext, walletPage: Page) => Promise<void>
+
+// TODO: This runs at least twice. Should we cache it somehow?
+export function defineWalletSetup(fn: WalletSetupFunction) {
+  const hash = getWalletSetupFuncHash(fn)
+
+  return {
+    hash,
+    fn
+  }
+}
