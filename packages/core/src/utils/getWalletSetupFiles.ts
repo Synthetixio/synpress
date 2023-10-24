d224dbe7c518ed73e1f7afa89856064b70802790
Daniel Izdebski <spidiritsu@gmail.com>
2023-10-24 22:01:58 +0200

    ✨ feat(core): Add `getWalletSetupFiles` function

---
+++ packages/core/src/utils/getWalletSetupFiles.ts
@@ -0,0 +1,31 @@
+import fs from 'fs-extra'
+
+const SETUP_FILE_FILTER_REGEX = /\.setup\.(js|ts)$/
+
+export async function getWalletSetupFiles(walletSetupDirPath: string) {
+  await fs.access(walletSetupDirPath).catch((e) => {
+    // TODO: This should utilize the `e.code`.
+    // TODO: See: https://nodejs.org/api/fs.html#fsexistspath-callback
+    // TODO: See fix: https://stackoverflow.com/a/49562477
+    // TODO: Replace ALL occurrences of `fs.exists` with `fs.access`.
+    if (e instanceof Error && e.message.includes('ENOENT')) {
+      throw new Error(`[GetWalletSetupFiles] Wallet setup directory does not exist at ${walletSetupDirPath}`)
+    }
+
+    throw e
+  })
+
+  const setupFilesFilter = (file: string) => file.match(SETUP_FILE_FILTER_REGEX)
+  const fileList = (await fs.readdir(walletSetupDirPath)).filter(setupFilesFilter)
+
+  if (!fileList.length) {
+    throw new Error(
+      [
+        `[GetWalletSetupFiles] No wallet setup files found at ${walletSetupDirPath}`,
+        'Remember that all wallet setup files must end with `.setup.{js,ts}` extension!'
+      ].join('\n')
+    )
+  }
+
+  return fileList
+}
