58dc209fe595018a594706e67ecabcd749b5592b
Daniel Izdebski <spidiritsu@gmail.com>
2023-12-05 16:53:46 +0100

    🔥 chore: Remove `metamask-legacy` package

--- examples/metamask-legacy/src/example.ts
+++
@@ -1,5 +0,0 @@
-import { helpers, metamask, metamaskPages } from 'metamask-legacy'
-
-console.log('Current network:', helpers.getCurrentNetwork())
-console.log('Example selector:', metamaskPages.mainPage.mainPageElements.importAccount.importButton)
-console.log('MetaMask extension urls:', metamask.extensionUrls())
