b7ef438d6689d799e8d3079a122367b7db4d651a
Daniel Izdebski <spidiritsu@gmail.com>
2023-12-05 17:12:27 +0100

    🔥 chore: Remove `metamask-legacy` package (#1032)

--- wallets/metamask-legacy/src/index.js
+++
@@ -1,6 +0,0 @@
-import * as metamask from './commands/metamask.cjs'
-import * as playwright from './commands/playwright.cjs'
-import * as helpers from './helpers.cjs'
-import metamaskPages from './pages/index.js'
-
-export { helpers, metamask, playwright, metamaskPages }
--- wallets/metamask-legacy/src/pages/metamask/unlock-page.cjs
+++
@@ -1,9 +0,0 @@
-const unlockPage = '[data-testid="unlock-page"]'
-const passwordInput = `${unlockPage} #password`
-const unlockButton = `${unlockPage} .btn-default`
-
-module.exports.unlockPageElements = {
-  unlockPage,
-  passwordInput,
-  unlockButton
-}
