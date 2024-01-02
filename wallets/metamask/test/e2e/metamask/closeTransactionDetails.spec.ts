f0d2f98cfa5af2b5f4109f7c2684f0e6418b08e7
Daniel Izdebski <spidiritsu@gmail.com>
2024-01-02 16:51:24 +0100

    ♻️ refactor(mm): Remove `experimental` object

--- wallets/metamask/test/e2e/metamask/closeTransactionDetails.spec.ts
+++ wallets/metamask/test/e2e/metamask/closeTransactionDetails.spec.ts
@@ -8,11 +8,11 @@ test('should close transaction details', async ({ page, metamask, metamaskPage, 
   await connectToAnvil()
 
   await page.locator('#sendEIP1559Button').click()
-  await metamask.experimental.confirmTransactionAndWaitForMining()
+  await metamask.confirmTransactionAndWaitForMining()
 
-  await metamask.experimental.openTransactionDetails(0)
+  await metamask.openTransactionDetails(0)
 
-  await metamask.experimental.closeTransactionDetails()
+  await metamask.closeTransactionDetails()
 
   await expect(metamaskPage.locator(metamask.homePage.selectors.popover.closeButton)).toBeHidden()
 })
