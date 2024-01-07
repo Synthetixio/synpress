0d15df9a853e50aa654c0a1e45ba442154a931af
Daniel Izdebski <spidiritsu@gmail.com>
2024-01-07 04:21:20 +0100

    💄 chore(docs): Update main page

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,14 +1,31 @@
+import { createRequire } from 'module'
 import { defineConfig } from 'vitepress'
+
+// TODO: Can we somehow change this to `import`?
+const require = createRequire(import.meta.url)
+const pkg = require('../../release/package.json')
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: 'My Awesome Project',
-  description: 'A VitePress Site',
+  cleanUrls: true,
+  title: 'Synpress',
+  description: 'E2E testing library for Web3 dapps.',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Docs', link: '/docs' },
+      { text: 'API', link: '/markdown-examples' },
+      { text: 'Examples', link: 'https://github.com/Synthetixio/synpress/tree/new-dawn/examples' },
+      {
+        text: pkg.version,
+        items: [
+          // TODO: Add changelog
+          {
+            text: 'Check out our Discord!',
+            link: 'https://discord.gg/XhZKSRGtWc'
+          }
+        ]
+      }
     ],
 
     sidebar: [
@@ -21,6 +38,13 @@ export default defineConfig({
       }
     ],
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }]
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Synthetixio/synpress/tree/new-dawn' },
+      { icon: 'discord', link: 'https://discord.gg/XhZKSRGtWc' },
+      { icon: 'x', link: 'https://twitter.com/Synpress_' }
+    ]
   }
+  // TODO: Add real a favicon
+  // TODO: Check if we should add other things into head
+  // head: [['link', { rel: 'icon', href: '/public/synpress-logo.png' }]]
 })
