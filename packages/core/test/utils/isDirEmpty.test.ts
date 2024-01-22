f2fb5a9312b271f0e973e71359de976e7cba1853
Daniel Izdebski <spidiritsu@gmail.com>
2024-01-22 16:52:10 +0100

    ✨ feat(core): Add `isDirEmpty` util

---
+++ packages/core/test/utils/isDirEmpty.test.ts
@@ -0,0 +1,39 @@
+import path from 'node:path'
+import { fs, vol } from 'memfs'
+import { afterAll, afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { isDirEmpty } from '../../src/utils/isDirEmpty'
+
+const ROOT_DIR = '/tmp'
+const FILE_PATH = path.join(ROOT_DIR, 'file.txt')
+
+vi.mock('fs-extra', async () => {
+  return {
+    default: fs.promises
+  }
+})
+
+describe('isDirEmpty', () => {
+  afterAll(() => {
+    vi.resetAllMocks()
+  })
+
+  beforeEach(() => {
+    vol.mkdirSync(ROOT_DIR)
+  })
+
+  afterEach(() => {
+    vol.reset() // Clear the in-memory file system after each test
+  })
+
+  it('returns `true` if the directory is empty', async () => {
+    const isEmpty = await isDirEmpty(ROOT_DIR)
+    expect(isEmpty).toEqual(true)
+  })
+
+  it('returns `false` if the directory contains files', async () => {
+    fs.writeFileSync(FILE_PATH, 'Hello there! 👋')
+
+    const isEmpty = await isDirEmpty(ROOT_DIR)
+    expect(isEmpty).toEqual(false)
+  })
+})
