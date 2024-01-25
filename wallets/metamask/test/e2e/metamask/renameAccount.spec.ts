fd70daa8bcbab391de207b23b3db1a7d02eaf4e6
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-01-25 07:57:40 -0500

    test

--- wallets/metamask/test/e2e/metamask/renameAccount.spec.ts
+++ wallets/metamask/test/e2e/metamask/renameAccount.spec.ts
@@ -1,5 +1,6 @@
 import { testWithSynpress } from '@synthetixio/synpress-fixtures'
 import { MetaMask, unlockForFixture } from '../../../src'
+import { z } from 'zod'
 
 import basicSetup from '../wallet-setup/basic.setup'
 
@@ -16,10 +17,14 @@ test('should rename current account with specified name', async ({ context, meta
   await expect(metamaskPage.locator(metamask.homePage.selectors.accountMenu.accountButton)).toHaveText(accountName)
 })
 
-test('should throw an error if an empty account name is passed', async ({ context, metamaskPage }) => {
+test('should throw Zod error if an empty account name is passed', async ({ context, metamaskPage }) => {
   const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword)
 
-  await expect(metamask.renameAccount('')).rejects.toThrowError(
-    '[RenameAccount] Account name cannot be an empty string'
-  )
+  await expect(metamask.renameAccount('')).rejects.toThrowError(z.ZodError)
+})
+
+test('should throw Zod error if a reserved account name is passed', async ({ context, metamaskPage }) => {
+  const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword)
+
+  await expect(metamask.renameAccount('Account 12')).rejects.toThrowError(z.ZodError)
 })
