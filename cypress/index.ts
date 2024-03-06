87f09bdc205d847b032ce0e092d75d1ecd258833
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-03-06 05:43:30 -0500

    cypress integration

---
+++ cypress/index.ts
@@ -0,0 +1,31 @@
+import { configure } from '@testing-library/cypress';
+import './commands';
+
+configure({ testIdAttribute: 'data-testid' });
+
+// dont fail tests on uncaught exceptions of websites
+Cypress.on('uncaught:exception', () => {
+  if (!Cypress.env('FAIL_ON_ERROR')) {
+    return false;
+  }
+});
+
+Cypress.on('window:before:load', win => {
+  cy.stub(win.console, 'error').callsFake(message => {
+    cy.now('task', 'error', message);
+    // fail test on browser console error
+    if (Cypress.env('FAIL_ON_ERROR')) {
+      throw new Error(message);
+    }
+  });
+
+  cy.stub(win.console, 'warn').callsFake(message => {
+    cy.now('task', 'warn', message);
+  });
+});
+
+before(() => {
+  if (!Cypress.env('SKIP_METAMASK_SETUP')) {
+    cy.setupMetamask();
+  }
+});
