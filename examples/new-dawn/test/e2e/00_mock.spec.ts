3f76c0748380c069b73a9fb3253aed005139b571
matstyler <mat.franczuk@gmail.com>
2024-04-08 21:30:26 +0200

    fix: dependencies

--- examples/new-dawn/test/e2e/00_mock.spec.ts
+++ examples/new-dawn/test/e2e/00_mock.spec.ts
@@ -4,13 +4,20 @@ const test = testWithMetaMaskMock
 
 const { expect } = test
 
-test('should work to the MetaMask Test Dapp', async ({ page }) => {
-  const metamask = new MetaMaskMock(page)
-
+test('should mock MetaMask in the Test Dapp', async ({ page }) => {
   await page.goto('/')
 
   await expect(page.locator('#accounts')).toHaveText('0xd73b04b0e696b0945283defa3eee453814758f1a')
 
   await page.locator('#getAccounts').click()
   await expect(page.locator('#getAccountsResult')).toHaveText('0xd73b04b0e696b0945283defa3eee453814758f1a')
+})
+
+test('should add new account using MetaMask mock', async ({ page }) => {
+  const metamask = new MetaMaskMock(page)
+
+  metamask.importWallet('candy maple cake sugar pudding cream honey rich smooth crumble sweet treat')
+  await metamask.addNewAccount()
+
+  await expect(page.locator('#accounts')).toHaveText('0xd73b04b0e696b0945283defa3eee453814758f1a')
 })
