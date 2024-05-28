45d1d6d0e0d02909e12b0b03ae9d7ade8f34e015
Harsh R <53080940+fullstackninja864@users.noreply.github.com>
2024-05-28 19:45:56 +0530

    fix: fixed metamask commands to support v11.15.5 (#1132)
    * release: v3.7.2-beta.9
    
    Signed-off-by: Jakub <ci@drptbl.dev>
    
    * feature: updated lib to support metamask 11.15.5 version
    
    * use latest pnpm
    
    Signed-off-by: drptbl <jakub.mucha@icloud.com>
    
    * update metamask version
    
    Signed-off-by: drptbl <jakub.mucha@icloud.com>
    
    * added packageManager in package.json
    
    * textcase fix
    
    * click on proceedAnyway
    
    Signed-off-by: r3kt.eth <so@r3kt.io>
    
    * add pnpm
    
    Signed-off-by: r3kt.eth <so@r3kt.io>
    
    * version update
    
    Signed-off-by: r3kt.eth <so@r3kt.io>
    
    ---------
    
    Signed-off-by: Jakub <ci@drptbl.dev>
    Signed-off-by: drptbl <jakub.mucha@icloud.com>
    Signed-off-by: r3kt.eth <so@r3kt.io>
    Co-authored-by: Jakub <ci@drptbl.dev>
    Co-authored-by: Harsh Rathi <harsh@FV3P2P7KXV.local>
    Co-authored-by: drptbl <jakub.mucha@icloud.com>
    Co-authored-by: r3kt.eth <so@r3kt.io>

--- pages/metamask/notification-page.js
+++ pages/metamask/notification-page.js
@@ -2,14 +2,14 @@ const notificationPage = '.notification';
 const notificationAppContent = `${notificationPage} #app-content .app`;
 const loadingLogo = `${notificationPage} #loading__logo`;
 const loadingSpinner = `${notificationPage} #loading__spinner`;
-const nextButton = `${notificationPage} .permissions-connect-choose-account__bottom-buttons .btn-primary`;
-const cancelButton = `${notificationPage} .permissions-connect-choose-account__bottom-buttons .btn-secondary`;
+const nextButton = `${notificationPage} [data-testid="page-container-footer-next"]`;
+const cancelButton = `${notificationPage} [data-testid="page-container-footer-cancel"]`;
 const customSpendingLimitInput = `${notificationPage} [data-testid="custom-spending-cap-input"]`;
 const allowToSpendButton = `${notificationPage} [data-testid="page-container-footer-next"]`;
 const rejectToSpendButton = `${notificationPage} [data-testid="page-container-footer-cancel"]`;
-const selectAllCheckbox = `${notificationPage} .choose-account-list__header-check-box`;
+const selectAllCheckbox = `${notificationPage} [data-testid="choose-account-list-operate-all-check-box"]`;
 const approveWarningToSpendButton = `${notificationPage} .set-approval-for-all-warning__footer__approve-button`;
-const rejectWarningToSpendButton = `${notificationPage} .btn-secondary.set-approval-for-all-warning__footer__cancel-button`;
+const rejectWarningToSpendButton = `${notificationPage} .set-approval-for-all-warning__footer__cancel-button`;
 
 module.exports.notificationPageElements = {
   notificationPage,
@@ -26,8 +26,8 @@ module.exports.notificationPageElements = {
   rejectWarningToSpendButton,
 };
 
-const confirmSignatureRequestButton = `${notificationPage} .request-signature__footer__sign-button`;
-const rejectSignatureRequestButton = `${notificationPage} .request-signature__footer__cancel-button`;
+const confirmSignatureRequestButton = `${notificationPage} [data-testid="page-container-footer-next"]`;
+const rejectSignatureRequestButton = `${notificationPage} [data-testid="page-container-footer-cancel"]`;
 const signatureRequestScrollDownButton = `${notificationPage} [data-testid="signature-request-scroll-button"]`;
 module.exports.signaturePageElements = {
   confirmSignatureRequestButton,
@@ -35,8 +35,8 @@ module.exports.signaturePageElements = {
   signatureRequestScrollDownButton,
 };
 
-const confirmDataSignatureRequestButton = `${notificationPage} [data-testid="signature-sign-button"]`;
-const rejectDataSignatureRequestButton = `${notificationPage} [data-testid="signature-cancel-button"]`;
+const confirmDataSignatureRequestButton = `${notificationPage} [data-testid="page-container-footer-next"]`;
+const rejectDataSignatureRequestButton = `${notificationPage} [data-testid="page-container-footer-cancel"]`;
 module.exports.dataSignaturePageElements = {
   confirmDataSignatureRequestButton,
   rejectDataSignatureRequestButton,
@@ -50,10 +50,10 @@ module.exports.permissionsPageElements = {
   connectButton,
 };
 
-const popupContainer = '.popover-container';
-const popupCloseButton = `${popupContainer} .popover-header__button`;
-const popupCopyRecipientPublicAddressButton = `${popupContainer} .nickname-popover__public-address button`;
-const recipientPublicAddress = `${popupContainer} .nickname-popover__public-address__constant`;
+const popupContainer = '.mm-modal';
+const popupCloseButton = `${popupContainer} [aria-label="Close"]`;
+const popupCopyRecipientPublicAddressButton = `${popupContainer} [aria-label="Copy address to clipboard"]`;
+const recipientPublicAddress = `${popupContainer} .mm-text-field--disabled input`;
 module.exports.recipientPopupElements = {
   popupContainer,
   popupCloseButton,
@@ -69,12 +69,12 @@ const recipientButton = `${confirmPageHeader} .sender-to-recipient__party--recip
 const recipientAddressTooltipContainerButton = `${confirmPageHeader} .sender-to-recipient__party--recipient .sender-to-recipient__tooltip-container`;
 const editGasFeeLegacyButton = `${notificationPage} .transaction-detail-edit button`;
 const editGasFeeLegacyOverrideAckButton = `${notificationPage} .edit-gas-display .edit-gas-display__dapp-acknowledgement-button`;
-const editGasLegacyPopup = `${notificationPage} .edit-gas-popover__wrapper`;
+const editGasLegacyPopup = `${notificationPage} .edit-gas-popover`;
 const advancedLegacyGasControls = `${editGasLegacyPopup} .edit-gas-display .advanced-gas-controls`;
 const gasLimitLegacyInput = `${advancedLegacyGasControls} .form-field:nth-child(1) input`;
 const gasPriceLegacyInput = `${advancedLegacyGasControls} .form-field:nth-child(2) input`;
 const saveLegacyButton = `${editGasLegacyPopup} .popover-footer .btn-primary`;
-const editGasFeeButton = `${notificationPage} [data-testid="edit-gas-fee-button"]`;
+const editGasFeeButton = `${notificationPage} [data-testid="edit-gas-fee-icon"]`;
 const gasOptionLowButton = `${notificationPage} [data-testid="edit-gas-fee-item-low"]`;
 const gasOptionMediumButton = `${notificationPage} [data-testid="edit-gas-fee-item-medium"]`;
 const gasOptionHighButton = `${notificationPage} [data-testid="edit-gas-fee-item-high"]`;
@@ -84,7 +84,8 @@ const baseFeeInput = `${notificationPage} [data-testid="base-fee-input"]`;
 const priorityFeeInput = `${notificationPage} [data-testid="priority-fee-input"]`;
 const editGasLimitButton = `${notificationPage} [data-testid="advanced-gas-fee-edit"]`;
 const gasLimitInput = `${notificationPage} [data-testid="gas-limit-input"]`;
-const saveCustomGasFeeButton = `${notificationPage} .popover-container .btn-primary`;
+const saveCustomGasFeeButton = `${notificationPage} .mm-box--background-color-primary-default`;
+const saveAdvanceCustomGasFeeButton = `${notificationPage} .advanced-gas-fee-popover .popover-footer button`;
 const totalLabel = `${confirmPageContent} .transaction-detail-item:nth-child(2) .transaction-detail-item__detail-values h6:nth-child(2)`; // todo: fix
 const customNonceInput = `${confirmPageContent} .custom-nonce-input input`;
 const tabs = `${confirmPageContent} .tabs`;
@@ -122,6 +123,7 @@ module.exports.confirmPageElements = {
   editGasLimitButton,
   gasLimitInput,
   saveCustomGasFeeButton,
+  saveAdvanceCustomGasFeeButton,
   totalLabel,
   customNonceInput,
   tabs,
@@ -135,15 +137,15 @@ module.exports.confirmPageElements = {
   confirmButton,
 };
 
-const confirmEncryptionPublicKeyButton = `${notificationPage} .request-encryption-public-key__footer__sign-button`;
-const rejectEncryptionPublicKeyButton = `${notificationPage} .request-encryption-public-key__footer__cancel-button`;
+const confirmEncryptionPublicKeyButton = `${notificationPage} [data-testid="page-container-footer-next"]`;
+const rejectEncryptionPublicKeyButton = `${notificationPage} [data-testid="page-container-footer-cancel"]`;
 module.exports.encryptionPublicKeyPageElements = {
   confirmEncryptionPublicKeyButton,
   rejectEncryptionPublicKeyButton,
 };
 
-const confirmDecryptionRequestButton = `${notificationPage} .request-decrypt-message__footer__sign-button`;
-const rejectDecryptionRequestButton = `${notificationPage} .request-decrypt-message__footer__cancel-button`;
+const confirmDecryptionRequestButton = `${notificationPage} [data-testid="page-container-footer-next"]`;
+const rejectDecryptionRequestButton = `${notificationPage} [data-testid="page-container-footer-cancel"]`;
 module.exports.decryptPageElements = {
   confirmDecryptionRequestButton,
   rejectDecryptionRequestButton,
