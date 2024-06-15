cc5265308d401ef094d21992e80bf2df798ba1fc
matstyler <mat.franczuk@gmail.com>
2024-06-15 23:42:34 +0200

    fix: cleanup

--- wallets/ethereum-wallet-mock/src/cypress/utils/installSynpress.ts
+++ wallets/ethereum-wallet-mock/src/cypress/utils/installSynpress.ts
@@ -20,9 +20,6 @@ export default function installSynpress(on: Cypress.PluginEvents, config: Cypres
     await initEthereumWalletMock(port)
   })
 
-  // TODO: Implement tasks if needed
-  // setupTasks(on);
-
   return {
     ...config,
     browsers
