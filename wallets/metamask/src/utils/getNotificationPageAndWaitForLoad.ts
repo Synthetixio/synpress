c45d1b20414eec5add2e22044cadd20f94397188
drptbl <jakub.mucha@icloud.com>
2024-06-20 12:49:26 +0100

    feat: wait for loading indicators to disappear in notification page
    Signed-off-by: drptbl <jakub.mucha@icloud.com>

--- wallets/metamask/src/utils/getNotificationPageAndWaitForLoad.ts
+++ wallets/metamask/src/utils/getNotificationPageAndWaitForLoad.ts
@@ -1,4 +1,5 @@
 import type { BrowserContext, Page } from '@playwright/test'
+import { errors } from '@playwright/test'
 
 export async function getNotificationPageAndWaitForLoad(context: BrowserContext, extensionId: string) {
   const notificationPageUrl = `chrome-extension://${extensionId}/notification.html`
@@ -20,7 +21,36 @@ export async function getNotificationPageAndWaitForLoad(context: BrowserContext,
     height: 592
   })
 
-  await notificationPage.waitForLoadState('load')
+  await notificationPage.waitForLoadState('domcontentloaded')
+
+  const loadingIndicators = [
+    '.loading-logo',
+    '.loading-spinner',
+    '.loading-overlay',
+    '.loading-overlay__spinner',
+    '.loading-span',
+    '.loading-indicator',
+    '#loading__logo',
+    '#loading__spinner',
+    '.mm-button-base__icon-loading',
+    '.loading-swaps-quotes',
+    '.loading-heartbeat'
+  ]
+
+  const timeout = 5000
+  for (const selector of loadingIndicators) {
+    try {
+      console.log(`Loading indicator '${selector}' found, waiting for it to disappear`)
+      await notificationPage.waitForSelector(selector, { state: 'hidden', timeout })
+    } catch (error) {
+      if (error instanceof errors.TimeoutError) {
+        console.log(`Loading indicator '${selector}' not found - continuing.`)
+      } else {
+        console.log(`Error while waiting for loading indicator '${selector}' to disappear`)
+        throw error
+      }
+    }
+  }
 
   return notificationPage
 }
