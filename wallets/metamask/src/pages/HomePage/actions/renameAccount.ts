8e9056ccbce1ece8aae5b0f8acbaa1e3b402fbca
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-06-20 09:23:53 -0400

    remove test

--- wallets/metamask/src/pages/HomePage/actions/renameAccount.ts
+++ wallets/metamask/src/pages/HomePage/actions/renameAccount.ts
@@ -1,19 +1,7 @@
 import type { Page } from '@playwright/test'
-import { z } from 'zod'
 import Selectors from '../selectors'
 
-// @todo, look into validation schema that's language agnostic for Metamask reserved words (""Account 1", "Account 2", etc.)
-const Account = z.object({
-  name: z.string().refine((value) => {
-    return value.trim().length > 0
-  }, 'Invalid account name')
-})
-
-export type Account = z.infer<typeof Account>
-
-export async function renameAccount(page: Page, newAccountName: Account['name']) {
-  Account.parse({ name: newAccountName })
-
+export async function renameAccount(page: Page, newAccountName: string) {
   await page.locator(Selectors.accountMenu.accountButton).click()
   await page.locator(Selectors.accountMenu.renameAccountMenu.listItemButton).nth(0).click()
   await page.locator(Selectors.threeDotsMenu.accountDetailsButton).click()
--- wallets/metamask/test/e2e/renameAccount.spec.ts
+++ wallets/metamask/test/e2e/renameAccount.spec.ts
@@ -7,17 +7,11 @@ const test = testWithSynpress(metaMaskFixtures(basicSetup))
 
 const { expect } = test
 
-test('should rename current account with specified name', async ({ context, metamaskPage }) => {
+test.only('should rename current account with specified name', async ({ context, metamaskPage }) => {
   const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword)
 
   const accountName = 'Test Account'
   await metamask.renameAccount(accountName)
 
   await expect(metamaskPage.locator(metamask.homePage.selectors.accountMenu.accountButton)).toHaveText(accountName)
-})
-
-test('should throw Zod error if an empty account name is passed', async ({ context, metamaskPage }) => {
-  const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword)
-
-  await expect(metamask.renameAccount('')).rejects.toThrowError('Invalid account name')
 })
