988f112af33015ba466bd587f713019b33d79d8b
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-06-24 07:15:43 -0400

    fix

--- release/src/index.ts
+++ release/src/index.ts
@@ -1,4 +1,3 @@
-import { EthereumWalletMock, ethereumWalletMockFixtures } from '@synthetixio/ethereum-wallet-mock'
 import { testWithSynpress } from '@synthetixio/synpress-core'
 import { MetaMask, getExtensionId, metaMaskFixtures } from '@synthetixio/synpress-metamask'
 import { defineWalletSetup } from '@synthetixio/synpress-utils'
@@ -6,11 +5,9 @@ import { defineWalletSetup } from '@synthetixio/synpress-utils'
 export {
   // Framework fixtures
   testWithSynpress,
-  ethereumWalletMockFixtures,
   metaMaskFixtures,
   // API
   MetaMask,
-  EthereumWalletMock,
   // Helpers
   defineWalletSetup,
   getExtensionId
