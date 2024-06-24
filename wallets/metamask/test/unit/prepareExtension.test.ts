630298b1d19b223733eb42da69ec28e6fea8b57f
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-06-24 12:26:11 -0400

    done

---
+++ wallets/metamask/test/unit/prepareExtension.test.ts
@@ -0,0 +1,40 @@
+import { afterAll, afterEach, describe, expect, it, vi } from 'vitest'
+import { prepareExtension } from '@synthetixio/synpress-cache'
+
+const MOCK_CACHE_DIR_PATH = 'mockCacheDirPath'
+const MOCK_EXTENSION_FINAL_PATH = 'mockExtensionFinalPath'
+
+vi.mock('@synthetixio/synpress-cache', async (importOriginal) => {
+  const prep: { prepareExtension: () => void } = await importOriginal()
+
+  return {
+    default: vi.fn(),
+    ensureCacheDirExists: vi.fn().mockImplementation(() => MOCK_CACHE_DIR_PATH),
+    downloadFile: vi.fn().mockImplementation(() => {
+      return {
+        filePath: 'mockExtensionArchivePath'
+      }
+    }),
+    unzipArchive: vi.fn().mockImplementation(() => {
+      return {
+        outputPath: MOCK_EXTENSION_FINAL_PATH
+      }
+    }),
+    prepareExtension: prep.prepareExtension
+  }
+})
+
+describe('prepareExtension', () => {
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
+  afterAll(() => {
+    vi.resetAllMocks()
+  })
+
+  it('returns correct unzipped extension path', async () => {
+    const extensionPath = await prepareExtension('MetaMask')
+    expect(extensionPath).toBeDefined()
+  })
+})
