cf89cc9deca3d8abdb023c882a2d3d944875944a
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-06-24 12:08:36 -0400

    fix and clean

--- wallets/metamask/playwright.config.ts
+++ wallets/metamask/playwright.config.ts
@@ -22,7 +22,6 @@ export default defineConfig({
 
   // Opt out of parallel tests on CI since it supports only 1 worker.
   workers: process.env.CI ? 1 : undefined,
-
   // Concise 'dot' for CI, default 'html' when running locally.
   // See https://playwright.dev/docs/test-reporters.
   reporter: process.env.CI
@@ -34,10 +33,9 @@ export default defineConfig({
   use: {
     // We are using locally deployed MetaMask Test Dapp.
     baseURL: 'http://localhost:9999',
-
     // Collect all traces on CI, and only traces for failed tests when running locally.
     // See https://playwright.dev/docs/trace-viewer.
-    trace: process.env.CI ? 'on' : 'retain-on-failure'
+    trace: process.env.CI ? 'on' : 'retain-on-failure',
   },
 
   // Configure projects for major browsers.
