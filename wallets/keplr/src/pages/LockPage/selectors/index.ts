6f6c783364d15ee7d61d6297d4420e63bb57bb65
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-07-01 09:52:30 -0400

    lint

--- wallets/keplr/src/pages/LockPage/selectors/index.ts
+++ wallets/keplr/src/pages/LockPage/selectors/index.ts
@@ -23,5 +23,5 @@ export const onboardingElements = {
   textAreaSelector: 'textbox',
   submitPhraseButton: 'button[type="submit"]',
   unlockPasswordInput: 'input',
-  phraseInput: 'input',
+  phraseInput: 'input'
 }
