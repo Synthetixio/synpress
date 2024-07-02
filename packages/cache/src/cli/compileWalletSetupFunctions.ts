b8645c4e8444e82845d7c775f9c37711aefd2a3b
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-07-02 07:32:21 -0400

    windows n mac glob

--- packages/cache/src/cli/compileWalletSetupFunctions.ts
+++ packages/cache/src/cli/compileWalletSetupFunctions.ts
@@ -6,29 +6,37 @@ import { FIXES_BANNER } from './compilationFixes'
 
 const OUT_DIR_NAME = 'wallet-setup-dist'
 
-const createGlobPattern = (walletSetupDir: string) => path.join(walletSetupDir, '**', '*.setup.{ts,js,mjs}')
+function escapeWindowsFilePath(filePath: string, platform: string) {
+  if (platform !== 'win32') return filePath;
+  return filePath.replace(/\\/g, '\\\\');
+}
 
-export async function compileWalletSetupFunctions(walletSetupDir: string, debug: boolean) {
-  const escapeWindowsFilePath = (filePath: string) => {
-    filePath.replace(/\//g, '\\')
-    const n = filePath.replace(/\\/g, '\\\\')
-    console.log(n, 'n', filePath)
-    return n
-  }
-  const outDir = path.join(ensureCacheDirExists(), OUT_DIR_NAME)
-  console.log(process.platform, 'platform')
-  const setupPath = process.platform === 'win32' ? escapeWindowsFilePath(walletSetupDir) : walletSetupDir
-  console.log(setupPath, 'setupPath')
-  const globPattern = createGlobPattern(setupPath)
-  const fullPath = path.join(process.cwd(), globPattern)
-  console.log(fullPath, 'fp')
-  const fileList = globSync('*.setup.{ts,js,mjs}')
-  console.log(fileList, 'fl')
+export async function compileWalletSetupFunctions(
+  walletSetupDir: string,
+  debug: boolean
+) {
+  const outDir = path.join(ensureCacheDirExists(), OUT_DIR_NAME);
+
+  // **Refined Glob Pattern:** 
+  const globPattern = path.join(walletSetupDir, '**', '*.setup.{ts,js,mjs}');
+  const platform = process.platform
+  // Get file paths using glob, ensuring proper path handling
+  const fileList = globSync(escapeWindowsFilePath(globPattern, platform));
+
   if (debug) {
-    console.log('[DEBUG] Found the following wallet setup files:')
-    console.log(fileList, globPattern, outDir, '\n')
+    console.log('[DEBUG] Found the following wallet setup files:');
+    console.log(fileList, '\n');
   }
 
+  // TODO: This error message is copied over from another function. Refactor this.
+  if (!fileList.length) {
+    throw new Error(
+      [
+        `No wallet setup files found at ${walletSetupDir}`,
+        'Remember that all wallet setup files must end with `.setup.{ts,js,mjs}` extension!'
+      ].join('\n')
+    );
+  }
   // TODO: This error message is copied over from another function. Refactor this.
   if (!fileList.length) {
     throw new Error(
