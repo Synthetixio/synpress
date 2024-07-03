7453accdcafbf679afd2b0a51bad9fe8f7e51fda
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-07-03 09:58:05 -0400

    remove unused

--- packages/cache/src/ensureCacheDirExists.ts
+++ packages/cache/src/ensureCacheDirExists.ts
@@ -1,11 +1,9 @@
 import path from 'node:path'
 import fs from 'fs-extra'
 import { CACHE_DIR_NAME } from './constants'
-// import appRoot from 'app-root-path'
 
 export function ensureCacheDirExists() {
   const cacheDirPath = path.join(process.cwd(), CACHE_DIR_NAME)
-  console.log(cacheDirPath)
   fs.ensureDirSync(cacheDirPath)
   return cacheDirPath
 }
