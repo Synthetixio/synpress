339fdab0ef7ad890469fbb62d1c6486e4d5a6176
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-07-08 11:00:48 -0400

    most actions done

--- wallets/phantom/src/pages/LockPage/actions/unlock.ts
+++ wallets/phantom/src/pages/LockPage/actions/unlock.ts
@@ -1,8 +1,9 @@
 import type { Page } from '@playwright/test'
 import { lockPageElements } from "../selectors";
+import { closePopupAndTooltips } from '../../HomePage/actions/closePopupAndTooltips';
 
-export const unlock = async (page: Page, password: string, close = false) => {
-  console.log('close', close)
+export const unlock = async (page: Page, extensionId: string, password: string, close = false) => {
+
   await page.waitForLoadState('domcontentloaded')
   
   const inputField = await page.locator(lockPageElements.unlockPageElements.passwordInput)
@@ -10,9 +11,9 @@ export const unlock = async (page: Page, password: string, close = false) => {
 
   await page.click(lockPageElements.unlockPageElements.unlockButton);
 
-  // if(close) {
-  //   await module.exports.closePopupAndTooltips();
-  // }
+  if(close) {
+    await closePopupAndTooltips(page, extensionId);
+  }
   
   return true;
 }
--- wallets/phantom/src/pages/NotificationPage/actions/lock.ts
+++ wallets/phantom/src/pages/NotificationPage/actions/lock.ts
@@ -1,13 +1,14 @@
 import type { Page } from '@playwright/test'
 import { notificationPageElements } from '../selectors'
 import { settingsPageElements } from '../../SettingsPage/selectors'
+import { closePopupAndTooltips } from '../../HomePage/actions/closePopupAndTooltips'
 
-export const lock = async (page: Page) => {
+export const lock = async (page: Page, extensionId: string) => {
   await page.waitForLoadState('domcontentloaded')
   await page.click(notificationPageElements.menu.buttons.settings);
   await page.click(notificationPageElements.menu.buttons.sidebar.settings)
   await page.click(settingsPageElements.buttons.lockWallet)
 
-  // await module.exports.closePopupAndTooltips();
+  await closePopupAndTooltips(page, extensionId);
   return true;
 }
