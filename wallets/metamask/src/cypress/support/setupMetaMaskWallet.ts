3f9e804ff423368a7640e342e08a3aefcbc9af6b
matstyler <mat.franczuk@gmail.com>
2024-07-08 21:44:22 +0200

    fix: gitignore cleanup

--- wallets/metamask/src/cypress/support/setupMetaMaskWallet.ts
+++ wallets/metamask/src/cypress/support/setupMetaMaskWallet.ts
@@ -17,9 +17,6 @@ export default async function setupMetaMaskWallet(port: number) {
   // First page (index equal 0) is the cypress page, second one (index equal 1) is the extension page
   const extensionPage = context.pages()[1] as Page
 
-  // await context.waitForEvent('page', { timeout: 5000 })
-  console.log(context.pages().length)
-
   await importWallet(extensionPage, SEED_PHRASE, 'password')
 
   await extensionPage.close()
