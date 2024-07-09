02d451e9a8d99e80651d8d173ee4852d3e461727
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-07-09 05:16:48 -0400

    ✨ feat: Run on windows with cache (#1161)
    * testing approot path
    
    * windows set up
    
    * glob
    
    * windows n mac glob
    
    * surpassed globby
    
    * working on windows (:
    
    * template literals
    
    * remove commented out
    
    * remove unused
    
    * no need for debug
    
    ---------
    
    Co-authored-by: sero <sero@synpress.io>

--- packages/cache/src/cli/compileWalletSetupFunctions.ts
+++ packages/cache/src/cli/compileWalletSetupFunctions.ts
@@ -1,4 +1,4 @@
-import path from 'node:path'
+import path, { posix, win32 } from 'node:path'
 import { glob } from 'glob'
 import { build } from 'tsup'
 import { ensureCacheDirExists } from '../ensureCacheDirExists'
@@ -6,13 +6,14 @@ import { FIXES_BANNER } from './compilationFixes'
 
 const OUT_DIR_NAME = 'wallet-setup-dist'
 
-const createGlobPattern = (walletSetupDir: string) => path.join(walletSetupDir, '**', '*.setup.{ts,js,mjs}')
-
 export async function compileWalletSetupFunctions(walletSetupDir: string, debug: boolean) {
   const outDir = path.join(ensureCacheDirExists(), OUT_DIR_NAME)
 
-  const globPattern = createGlobPattern(walletSetupDir)
-  const fileList = await glob(globPattern)
+  // Use a normalized glob pattern
+  const globPattern = path.join(walletSetupDir, '**', '*.setup.{ts,js,mjs}')
+
+  // Use glob to find files, ensuring proper path handling
+  const fileList: string[] = await glob(globPattern, { absolute: false, windowsPathsNoEscape: true })
 
   if (debug) {
     console.log('[DEBUG] Found the following wallet setup files:')
@@ -29,27 +30,34 @@ export async function compileWalletSetupFunctions(walletSetupDir: string, debug:
     )
   }
 
-  await build({
-    name: 'cli-build',
-    silent: true,
-    entry: fileList,
-    clean: true,
-    outDir,
-    format: 'esm',
-    splitting: true,
-    sourcemap: false,
-    config: false,
-    // TODO: Make this list configurable.
-    external: ['@synthetixio/synpress', '@playwright/test', 'playwright-core', 'esbuild', 'tsup'],
-    banner: {
-      js: FIXES_BANNER
-    },
-    esbuildOptions(options) {
-      // TODO: In this step, if the debug file is present, we should modify `console.log` so it prints from which file the log is coming from.
-      // We're dropping `console.log` and `debugger` statements because they do not play nicely with the Playwright Test Runner.
-      options.drop = debug ? [] : ['console', 'debugger']
-    }
+  const normalized = fileList.map((file) => {
+    return file.split(win32.sep).join(posix.sep)
   })
 
+  try {
+    await build({
+      name: 'cli-build',
+      silent: true,
+      entry: normalized,
+      clean: true,
+      outDir,
+      format: 'esm',
+      splitting: true,
+      sourcemap: false,
+      config: false,
+      // TODO: Make this list configurable.
+      external: ['@synthetixio/synpress', '@playwright/test', 'playwright-core', 'esbuild', 'tsup'],
+      banner: {
+        js: FIXES_BANNER
+      },
+      esbuildOptions(options) {
+        // TODO: In this step, if the debug file is present, we should modify `console.log` so it prints from which file the log is coming from.
+        // We're dropping `console.log` and `debugger` statements because they do not play nicely with the Playwright Test Runner.
+        options.drop = debug ? [] : ['console', 'debugger']
+      }
+    })
+  } catch (e) {
+    console.log('error within compile', e)
+  }
   return outDir
 }
