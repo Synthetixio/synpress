35635e2041a70ab8fd3f0ed49f97ea3139bf64ba
matstyler <mat.franczuk@gmail.com>
2024-07-09 12:23:58 +0200

    fix: cleanup

--- wallets/metamask/test/unit/prepareExtension.test.ts
+++ wallets/metamask/test/unit/prepareExtension.test.ts
@@ -1,7 +1,7 @@
 import * as core from '@synthetixio/synpress-cache'
 import { afterAll, afterEach, describe, expect, it, vi } from 'vitest'
 
-import { prepareExtension, DEFAULT_METAMASK_VERSION, EXTENSION_DOWNLOAD_URL } from '../../src/prepareExtension'
+import { DEFAULT_METAMASK_VERSION, EXTENSION_DOWNLOAD_URL, prepareExtension } from '../../src/prepareExtension'
 
 const MOCK_CACHE_DIR_PATH = 'mockCacheDirPath'
 const MOCK_EXTENSION_ARCHIVE_PATH = 'mockExtensionArchivePath'
