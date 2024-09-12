ed2914ef2d5075cc7821277d2364fcaffdaab060
matstyler <mat.franczuk@gmail.com>
2024-09-12 11:20:47 +0200

    feat: confirmTransactionAndWaitForMining using Cypress

--- wallets/metamask/test/cypress/connectToDapp.cy.ts
+++ wallets/metamask/test/cypress/connectToDapp.cy.ts
@@ -6,5 +6,5 @@ it('should connect account to the app', () => {
   cy.get('#connectButton').click()
   cy.connectToDapp()
 
-  cy.get('#accounts').should('contain', '0x')
+  cy.get('#accounts').should('include', /^0x/)
 })
