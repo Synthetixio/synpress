598a982136b6d5649694c61c96a2c67b0b3471fb
Jakub Adamczyk <jakub.adamczyk@phantom.app>
2024-10-03 11:00:03 +0200

    test new changes

--- pages/phantom/first-time-flow-page.js
+++ pages/phantom/first-time-flow-page.js
@@ -15,7 +15,7 @@ module.exports.metametricsPageElements = {
 };
 
 const firstTimeFlowPage = '.first-time-flow';
-const importWalletButton = `text=Import an existing wallet`;
+const importWalletButton = `text=I already have a wallet`;
 const importRecoveryPhraseButton = `text=Import Secret Recovery Phrase`;
 const createWalletButton = `${firstTimeFlowPage} [data-testid="create-wallet-button"]`;
 module.exports.firstTimeFlowPageElements = {
