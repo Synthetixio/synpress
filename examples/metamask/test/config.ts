73a59ce0c5c89477d1e78fac30eb70a94fe8bfa8
matstyler <mat.franczuk@gmail.com>
2024-12-11 10:36:38 +0100

    fix: cleanup

--- examples/metamask/test/config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  baseUrl: 'https://www.alphabot.app/',
-  seedPhrase: 'test test test test test test test test test test test junk',
-  metamaskPassword: 'SynpressIsAwesomeNow!!!'
-}
